3db3c63cf4647e47af3cbd452140d894e3abe1bf
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-09-18 01:37:25 -0400

    servr at end

--- render.R
+++ render.R
@@ -2,7 +2,7 @@ options(repos="http://cloud.r-project.org")
 for(p in c("penaltyLearning","future.apply","maps","lars","LambertW","kernlab","data.table","quarto"))if(!requireNamespace(p))install.packages(p)
 remotes::install_github("animint/animint2")
 quarto::quarto_render("chapters")
-if(interactive())servr::httd("chapters/_book/")
+## copy data viz to site.
 animint_js_vec <- Sys.glob("chapters/*/*/animint.js")
 from_dir_vec <- dirname(animint_js_vec)
 to_dir_vec <- dirname(sub("/", "/_book/", from_dir_vec))
@@ -11,4 +11,5 @@ for(to_dir in names(from_to_list)){
   from_dir <- from_to_list[[to_dir]]
   file.copy(from_dir, to_dir, recursive=TRUE)
 }
-
+## preview site.
+if(interactive())servr::httd("chapters/_book")
