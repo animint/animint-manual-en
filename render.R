8cd2c29d8b3b6369ab0e1dd8a4ae5935c6ca7721
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2025-09-26 10:48:34 -0400

    restore unlink

--- render.R
+++ render.R
@@ -1,6 +1,7 @@
 options(repos="http://cloud.r-project.org")
 for(p in c("penaltyLearning","future.apply","maps","lars","LambertW","kernlab","data.table","quarto"))if(!requireNamespace(p))install.packages(p)
 remotes::install_github("animint/animint2@screenshot-in-rmd")
+unlink("chapters/_book", recursive = TRUE)
 quarto::quarto_render("chapters")
 ## copy data viz to site.
 for(glob in c("chapters/*/animint.js", "chapters/Ch*/*/animint.js")){
