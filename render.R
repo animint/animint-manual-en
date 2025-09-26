b6bd0dbff4fdc7055f990ee5bd1042a9fd9e6ec4
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2025-09-26 10:47:44 -0400

    restore glob copy code

--- render.R
+++ render.R
@@ -2,14 +2,17 @@ options(repos="http://cloud.r-project.org")
 for(p in c("penaltyLearning","future.apply","maps","lars","LambertW","kernlab","data.table","quarto"))if(!requireNamespace(p))install.packages(p)
 remotes::install_github("animint/animint2@screenshot-in-rmd")
 quarto::quarto_render("chapters")
-animint_js_vec <- Sys.glob("chapters/*/*/animint.js")
-from_dir_vec <- dirname(animint_js_vec)
-to_dir_vec <- dirname(sub("/", "/_book/", from_dir_vec))
-from_to_list <- split(from_dir_vec, to_dir_vec)
-for(to_dir in names(from_to_list)){
-  from_dir <- from_to_list[[to_dir]]
-  file.copy(from_dir, to_dir, recursive=TRUE)
+## copy data viz to site.
+for(glob in c("chapters/*/animint.js", "chapters/Ch*/*/animint.js")){
+  animint_js_vec <- Sys.glob(glob)
+  from_dir_vec <- dirname(animint_js_vec)
+  to_dir_vec <- dirname(sub("/", "/_book/", from_dir_vec))
+  from_to_list <- split(from_dir_vec, to_dir_vec)
+  for(to_dir in names(from_to_list)){
+    from_dir <- from_to_list[[to_dir]]
+    print(to_dir)
+    file.copy(from_dir, to_dir, recursive=TRUE)
+  }
 }
-file.copy("chapters/Ch01vizKeeling", "chapters/_book", recursive=TRUE)
 ## preview site.
 if(interactive())servr::httd("chapters/_book")
