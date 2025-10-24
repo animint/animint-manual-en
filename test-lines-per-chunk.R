6a11a3ff29a94ea5bd434285a0cf75a3f3631328
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-10-23 23:50:33 -0400

    always show top 10

--- test-lines-per-chunk.R
+++ test-lines-per-chunk.R
@@ -21,13 +21,12 @@ for(qmd in qmd.files){
     shown_dt_list[[qmd]] <- data.table(qmd, this_shown)
   }
 }
-shown_dt <- rbindlist(shown_dt_list)[order(code_lines)]
+shown_dt <- rbindlist(shown_dt_list)[order(-code_lines)]
 print(shown_dt$code_lines)
-line.limit <- 100
-too_long <- shown_dt[code_lines>line.limit]
-for(row_i in 1:nrow(too_long)){
-  too_long[row_i, cat(sprintf(
-    "%4d / %4d %s %d lines > %d\n%s\n\n",
-    row_i, nrow(too_long), qmd, code_lines, line.limit, code_string))]
+for(row_i in 1:10){
+  shown_dt[row_i, cat(sprintf(
+    "%4d / %4d %s %d lines\n%s\n\n",
+    row_i, nrow(shown_dt), qmd, code_lines, code_string))]
 }
+too_long <- shown_dt[code_lines>100]
 q(status=nrow(too_long))
