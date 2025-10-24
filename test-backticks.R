992d6c3327349521cdd9b943071db0ab55599b1e
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2025-10-24 09:49:41 -0400

    geom function names with parens

--- test-backticks.R
+++ test-backticks.R
@@ -7,8 +7,13 @@ for(qmd in qmd.files){
   is.fence <- grepl("```", qmd.lines)
   is.text <- (cumsum(is.fence) %% 2)==0
   qmd.text <- qmd.lines[is.text]
-  bad.lines <- grep(
-    "[^`](?:clickSelects|showSelected)", qmd.text, value=TRUE)
+  geom.lines <- grep("geom_", qmd.text, value=TRUE, perl=TRUE)
+  if(FALSE){
+    geom.lines <- c("`geom_text`","geom_text","geom_text()","`geom_text()`")
+  }
+  bad.lines <- c(
+    grep("[^`](?:clickSelects|showSelected)", qmd.text, value=TRUE),
+    grep("geom_[^(]+\\(\\)`", geom.lines, value=TRUE, perl=TRUE, invert=TRUE))
   if(length(bad.lines)){
     violations[[qmd]] <- bad.lines
   }
